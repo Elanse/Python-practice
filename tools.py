61d92776c2ea229349b96500693cb3fbef3cc6be
Elanse <33039002+Elanse@users.noreply.github.com>
2019-01-09 13:49:59 -0800

    add

--- tools.py
+++ tools.py
@@ -5,3 +5,14 @@ def three_or_four(n):
         return True
     else:
         return False
+
+def test_long_word
+    if (len) > 8:
+        return True
+    if (len) <= 8:
+        return False
+    if (len) = 0:
+        return None
+
+def test_leap_year
+    if 
